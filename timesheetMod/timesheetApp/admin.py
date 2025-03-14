052df7759db2bd398a18ffaaa11eb5684dc3553d
Rishikesh <rishikesh2511990@gmail.com>
2025-03-14 13:21:16 +0530

    update: populated modules

--- timesheetMod/timesheetApp/admin.py
+++ timesheetMod/timesheetApp/admin.py
@@ -6,14 +6,14 @@ from .models import Project,Module,Task,Team
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
-    fieldsets = (
-        ("Basic Info", {"fields": ("name", "status")}),
-        ("Timestamps", {"fields": ("created_at", "updated_at")}),
-    )
+    # fieldsets = (
+    #     ("Basic Info", {"fields": ("name", "status")}),
+    #     ("Timestamps", {"fields": ("created_at", "updated_at")}),
+    # )
     readonly_fields = ["created_at", "updated_at"]
     list_editable = ["status"]
     list_display = ["project_id","name", "status", "formatted_created_at","formatted_updated_at"]
-    list_filter = ["name", "status", "created_at","updated_at"]
+    list_filter = ["status", "created_at","updated_at"]
     search_fields = ["name"]
     def formatted_created_at(self, obj):
         return localtime(obj.created_at).strftime("%d/%m/%y %H:%M:%S")
@@ -27,8 +27,27 @@ class ProjectAdmin(admin.ModelAdmin):
     formatted_updated_at.admin_order_field = "updated_at"
     formatted_updated_at.short_description = "Updated At"
 
+@admin.register(Module)
+class ModuleAdmin(admin.ModelAdmin):
+    list_display=["name","module_id","project","formatted_created_at","formatted_updated_at"]
+    readonly_fields = ["created_at", "updated_at"]
+    list_editable = ["project"]
+    list_filter = ["project","created_at","updated_at"]
+    search_fields = ["name","project__name"]
+
+    def formatted_created_at(self, obj):
+        return localtime(obj.created_at).strftime("%d/%m/%y %H:%M:%S")
+
+    def formatted_updated_at(self, obj):
+        return localtime(obj.updated_at).strftime("%d/%m/%y %H:%M:%S")
+
+    formatted_created_at.admin_order_field = "created_at"
+    formatted_created_at.short_description = "Created At"
+
+    formatted_updated_at.admin_order_field = "updated_at"
+    formatted_updated_at.short_description = "Updated At"
 
 # admin.site.register(Project,ProjectAdmin)
-admin.site.register(Module)
+# admin.site.register(Module)
 admin.site.register(Team)
 admin.site.register(Task)
