73c5fc9797af99c5d64bf170509f09fbd315e05e
Rishikesh <rishikesh2511990@gmail.com>
2025-03-25 12:28:30 +0530

    update: css done

--- timesheetMod/timesheetApp/urls.py
+++ timesheetMod/timesheetApp/urls.py
@@ -8,11 +8,11 @@ urlpatterns = [
     path("getProjects/", reads.get_projects, name="get_projects"),
     path("getModules/", reads.get_modules, name="get_modules"),
     path("getTasks/", reads.get_tasks, name="get_tasks"),
-    # django form views
-    path('entry/', views.entryForm, name='entry_form'),
+    # django form-crud views
+    path('create/', views.entryForm, name='entry_form'),
     path("entries/", reads.get_entries, name="get_entries"),
-    path('timesheet/',views.index2,name="index2"),
-    path('timesheet/updateForm/<int:entry_id>/', views.update_entry2, name="update_entries2"),
+    path('',views.index2,name="index2"),
+    path('update/<int:entry_id>/', views.update_entry2, name="update_entries2"),
     path("entries/delete/<int:entry_id>/",views.delete_entry, name="delete_entry"),
 
 ]
