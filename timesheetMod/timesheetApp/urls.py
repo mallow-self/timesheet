a2b4d8f1b1f4ecf8eb63a0cd2d95d5b26a24f90d
Rishikesh <rishikesh2511990@gmail.com>
2025-03-25 11:25:54 +0530

    remove: v1 version code removed

--- timesheetMod/timesheetApp/urls.py
+++ timesheetMod/timesheetApp/urls.py
@@ -8,12 +8,11 @@ urlpatterns = [
     path("getProjects/", reads.get_projects, name="get_projects"),
     path("getModules/", reads.get_modules, name="get_modules"),
     path("getTasks/", reads.get_tasks, name="get_tasks"),
-    path("entries/", reads.get_entries, name="get_entries"),
-    path("entries/delete/<int:entry_id>/", views.delete_entry, name="delete_entry"),
-    path("entries/update/<int:entry_id>/", views.update_entry, name="update_entries"),
-    path("updateForm/<int:entry_id>/", views.update_form, name="update_form"),
     # django form views
     path('entry/', views.entryForm, name='entry_form'),
+    path("entries/", reads.get_entries, name="get_entries"),
     path('timesheet/',views.index2,name="index2"),
     path('timesheet/updateForm/<int:entry_id>/', views.update_entry2, name="update_entries2"),
+    path("entries/delete/<int:entry_id>/",views.delete_entry, name="delete_entry"),
+
 ]
--- timesheetMod/timesheetApp/views.py
+++ timesheetMod/timesheetApp/views.py
@@ -15,69 +15,6 @@ def delete_entry(request, entry_id):
 
     return JsonResponse({"error": "Invalid request method"}, status=400)
 
-def update_entry(request, entry_id):
-    entry = get_object_or_404(Entry, entry_id=entry_id)
-    if request.method == "POST":
-        try:
-            date_entry = request.POST.get("date")
-            description = request.POST.get("description")
-            project_id = request.POST.get("project")
-            module_id = request.POST.get("module")
-            task_id = request.POST.get("task")
-            time_entry = request.POST.get("time")
-
-            if not all(
-                [date_entry, description, project_id, module_id, task_id, time_entry]
-            ):
-                return JsonResponse({"error": "All fields are required"}, status=400)
-            # Fetch related objects
-            project = get_object_or_404(Project, project_id=project_id)
-            module = get_object_or_404(Module, module_id=module_id)
-            task = get_object_or_404(Task, task_id=task_id)
-
-            # Convert time_entry to integer
-            time_entry = int(time_entry) if time_entry.isdigit() else 0
-
-            # check if task,module and project match
-            if int(task.module.module_id) == int(module_id) and int(task.module.project.project_id) == int(project_id):
-                # Update the todo object
-                entry.date_entry = date_entry
-                entry.description = description
-                entry.project = project
-                entry.module = module
-                entry.task = task
-                entry.time_entry = time_entry
-                entry.save()
-
-                return redirect("timesheet:index")
-            else:
-                return JsonResponse({"message:": "Invalid Project-Module-Task!"})
-
-        except Exception as e:
-            return JsonResponse({"server-error": f"{e}"})
-
-    return JsonResponse({"error": "Invalid request"}, status=400)
-
-def update_form(request,entry_id):
-    try:
-        entry = get_object_or_404(Entry, entry_id=entry_id)
-        date_entry_str = entry.date_entry.strftime("%Y-%m-%d") if entry.date_entry else ""
-        time_entry_id:int = 0
-        if entry.time_entry % 15 == 0:
-            time_entry_id = int(entry.time_entry // 15)
-        context = {
-            "entry_id": entry.entry_id,
-            "date_entry": date_entry_str,
-            "description": entry.description,
-            "project":entry.project,
-            "module":entry.module.name,
-            "task":entry.task,
-            "time_entry":entry.time_entry,
-        }
-        return render(request, "timesheetApp/update.html", context)
-    except Exception as e:
-        return JsonResponse({"Exception Occurred:": f"{e}"})
-
 #django model form
 def entryForm(request):
     if request.method == "POST":
